518acf955931c886a77ce73253e88daec61f164a
errabi ayoub <errabi.ayoub@gmail.com>
2024-11-13 23:46:28 +0100

    setup composite service part 2

--- product-composite-service/src/main/java/ma/errabi/microservice/composite/ProductCompositeIntegration.java
+++ product-composite-service/src/main/java/ma/errabi/microservice/composite/ProductCompositeIntegration.java
@@ -12,26 +12,39 @@ public class ProductCompositeIntegration implements ProductResource {
 
     private final RestTemplate restTemplate;
     private final ObjectMapper objectMapper;
-    private final String productServiceUrl;
-    private final String productReviewServiceUrl;
-    private final String productRecommendationServiceUrl;
+    private final String productServiceHost;
+    private final int productServicePort;
+    private final String productReviewServiceHost;
+    private final int productReviewServicePort ;
+    private final String productRecommendationServiceHost;
+    private final int productRecommendationServicePort;
 
     public ProductCompositeIntegration(RestTemplate restTemplate,
                                        ObjectMapper objectMapper,
                                        @Value("${app.product-service.host}")
-                                       String productServiceUrl,
+                                       String productServiceHost,
+                                       @Value("${app.product-service.port}")
+                                       int productServicePort,
                                        @Value("${app.product-review-service.host}")
-                                       String productReviewServiceUrl,
+                                       String productReviewServiceHost,
+                                       @Value("${app.product-review-service.port}")
+                                       int productReviewServicePort,
                                        @Value("${app.product-recommendation-service.host}")
-                                       String productRecommendationServiceUrl) {
+                                       String productRecommendationServiceHost,
+                                       @Value("${app.product-recommendation-service.port}")
+                                       int productRecommendationServicePort) {
         this.restTemplate = restTemplate;
         this.objectMapper = objectMapper;
-        this.productServiceUrl = productServiceUrl;
-        this.productReviewServiceUrl = productReviewServiceUrl;
-        this.productRecommendationServiceUrl = productRecommendationServiceUrl;
+        this.productServiceHost = String.format("%s:%d", productServiceHost, productServicePort);
+        this.productServicePort = productServicePort;
+        this.productReviewServiceHost = String.format("%s:%d", productReviewServiceHost, productReviewServicePort);
+        this.productRecommendationServiceHost = String.format("%s:%d", productRecommendationServiceHost, productRecommendationServicePort);
+        this.productReviewServicePort = productReviewServicePort;
+        this.productRecommendationServicePort = productRecommendationServicePort;
     }
     @Override
     public ProductDTO getProductById(Integer productId) {
-        return null;
+        String url = String.format(productServiceHost + "/product/%d", productId);
+        return restTemplate.getForObject(url, ProductDTO.class);
     }
 }
