879cec158026ade6498872c242c602d847b6d562
errabi ayoub <errabi.ayoub@gmail.com>
2025-06-03 16:29:13 +0100

    Setup http basic security for eureka server and client

---
+++ infrastructure/eureka-server/src/main/java/com/errabi/eureka/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.errabi.eureka.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Value("${app.eureka.username}")
+    private String username ;
+
+    @Value("${app.eureka.password}")
+    private String password ;
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        return new InMemoryUserDetailsManager(
+                org.springframework.security.core.userdetails.User.withUsername(username)
+                        .password(password)
+                        .roles("USER")
+                        .build()
+        );
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
